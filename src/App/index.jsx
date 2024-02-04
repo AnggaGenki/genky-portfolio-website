82f075d4446dba9ef4f3c3df6c40c24009aba147
AnggaGenki <genkiangga80@gmail.com>
2024-02-04 15:26:00 +0800

    2402040 - [New Nav] : Create navbar

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,89 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import "./index.scss";
 
 export default function App() {
-  return <Fragment></Fragment>;
+  const [soShowMenuBurger, setoShowMenuBurger] = useState({
+    status: false,
+    rotate: 0,
+    topLineTranslate: "0 0",
+    bottomLineTranslate: "0 0",
+    centerLineOpacity: 1,
+    backgroundColor: "white",
+  });
+
+  const fHandleMenuBurger = () => {
+    if (soShowMenuBurger.status) {
+      setoShowMenuBurger({
+        status: false,
+        rotate: 0,
+        topLineTranslate: "0, 0",
+        bottomLineTranslate: "0, 0",
+        centerLineOpacity: 1,
+        backgroundColor: "white",
+      });
+    } else {
+      setoShowMenuBurger({
+        status: true,
+        rotate: 45,
+        topLineTranslate: "4px, 4px",
+        bottomLineTranslate: "4px, -4px",
+        centerLineOpacity: 0,
+        backgroundColor: "black",
+      });
+    }
+  };
+
+  return (
+    <Fragment>
+      <style jsx="true">
+        {`
+          .container-navbar section:first-child button span:first-child {
+            transform: rotate(${soShowMenuBurger.rotate}deg)
+              translate(${soShowMenuBurger.topLineTranslate});
+            background-color: ${soShowMenuBurger.backgroundColor};
+          }
+
+          .container-navbar section:first-child button span:nth-child(2) {
+            opacity: ${soShowMenuBurger.centerLineOpacity};
+          }
+
+          .container-navbar section:first-child button span:last-child {
+            transform: rotate(-${soShowMenuBurger.rotate}deg)
+              translate(${soShowMenuBurger.bottomLineTranslate});
+            background-color: ${soShowMenuBurger.backgroundColor};
+          }
+        `}
+      </style>
+
+      <nav className="container-navbar">
+        <section>
+          <button onClick={fHandleMenuBurger} title="Burger Menu Button">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </section>
+        <header>
+          <h1>Home</h1>
+        </header>
+        <section>
+          <ul>
+            <li>
+              <button className="active-page">Home</button>
+            </li>
+            <li>
+              <button>Calculator</button>
+            </li>
+            <li>
+              <button>Game</button>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <button>Sign Up</button>
+          <button>Login</button>
+        </section>
+      </nav>
+    </Fragment>
+  );
 }
